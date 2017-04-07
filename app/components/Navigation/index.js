f10214da64e05a57e5c398a683511f2c20ffed17
sdonose <sidona_g@yahoo.com>
2017-04-07 10:55:57 +0300

    first commit

---
+++ app/components/Navigation/index.js
@@ -0,0 +1,20 @@
+/**
+*
+* Navigation
+*
+*/
+
+import React from 'react';
+
+
+import styles from './styles.css';
+
+function Navigation() {
+  return (
+    <div className={styles.navigation}>
+      This is the navigation
+    </div>
+  );
+}
+
+export default Navigation;
